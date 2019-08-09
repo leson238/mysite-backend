8b05ae1bbf88524fce5b48848ac9b6681b455130
Son Le <leson238@gmail.com>
2019-08-09 08:29:39 -0400

    First commit: -Set up mail contact and server responses

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var http = require("http");
+var index = require("./route");
+
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+const server = http.createServer(app);
+server.listen(3002);
